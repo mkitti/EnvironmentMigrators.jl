e6c7091d5ee6d868d47e48c39b78372a514a33fc
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-02-03 17:32:29 -0500

    Initial working commit

--- README.md
+++ README.md
@@ -1,3 +1,127 @@
 # EnvironmentMigrators
 
 [![Build Status](https://github.com/mkitti/EnvironmentMigrators.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/mkitti/EnvironmentMigrators.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+This package is to facilitate the copying of Julia shared environments, particularly between versions, as well as backing up of the current environment.
+
+# Installation
+
+In the Julia REPL,
+`] add EnvironmentMigrators`
+
+# Interactive Use
+
+For interactive use, invoke `EnvironmentMigrators.wizard()`.
+
+```julia
+
+julia> using EnvironmentMigrators
+
+julia> EnvironmentMigrators.wizard()
+Use the arrow keys to move the cursor. Press enter to select.
+Please select a shared environment to copy to C:\Users\KITTIS~1\AppData\Local\Temp\jl_CPGp07\Project.toml:
+   __pluto_boot_1.6.0
+   v1.5
+   v1.6
+ > v1.7(current version)
+   v1.8
+   Backup the current environment: C:\Users\KITTIS~1\AppData\Local\Temp\jl_CPGp07\Project.toml.
+   Quit. Do Nothing.
+[ Info: Current environment:
+      Status `C:\Users\kittisopikulm\AppData\Local\Temp\jl_CPGp07\Project.toml`
+  [e51d7f76] EnvironmentMigrators v0.1.0 `C:\Users\kittisopikulm\.julia\dev\EnvironmentMigrators`
+┌ Info: Backing up Project.toml and Manifest.toml
+│   current_env_project_toml = "C:\\Users\\KITTIS~1\\AppData\\Local\\Temp\\jl_CPGp07\\Project.toml"
+│   current_env_manifest_toml = "C:\\Users\\KITTIS~1\\AppData\\Local\\Temp\\jl_CPGp07\\Manifest.toml"
+└   backup_dir = "C:\\Users\\KITTIS~1\\AppData\\Local\\Temp\\jl_CPGp07\\backups\\2022-02-03_16_47_33"
+┌ Info: Copying selected Project.toml to current environment
+│   selected_project_toml = "C:\\Users\\kittisopikulm\\.julia\\environments\\v1.7\\Project.toml"
+└   current_env = "C:\\Users\\KITTIS~1\\AppData\\Local\\Temp\\jl_CPGp07"
+      Status `C:\Users\kittisopikulm\AppData\Local\Temp\jl_CPGp07\Project.toml
+      Status `C:\Users\kittisopikulm\AppData\Local\Temp\jl_CPGp07\Project.toml`
+  [6e4b80f9] BenchmarkTools
+  ...
+    Updating `C:\Users\kittisopikulm\AppData\Local\Temp\jl_CPGp07\Project.toml`
+  [6e4b80f9] + BenchmarkTools v1.2.2
+  ...
+    Updating `C:\Users\kittisopikulm\AppData\Local\Temp\jl_CPGp07\Manifest.toml`
+  [1520ce14] + AbstractTrees v0.3.4
+  [79e6a3ab] + Adapt v3.3.3
+  [4fba245c] + ArrayInterface v4.0.2
+  [6e4b80f9] + BenchmarkTools v1.2.2
+...
+[ Info: Migration successful
+      Status `C:\Users\kittisopikulm\AppData\Local\Temp\jl_CPGp07\Project.toml`
+...
+```
+
+```
+
+# Non-interactive Use
+
+```
+julia.exe -e '
+using Pkg
+Pkg.activate(mktempdir())
+Pkg.add(\"EnvironmentMigrators\")
+using EnvironmentMigrators
+EnvironmentMigrators.migrate_selected_environment(joinpath(DEPOT_PATH[1], \"environments\", \"v1.6\"))
+'
+```
+
+# Manually moving environments without this package
+
+To manually move environments, copy the `Project.toml` and `Manifest.toml` from the old environment to the new environment, 
+backing up Project.toml and Manifest.toml as needed. The default environments live in `joinpath(first(DEPOT_PATH), "environments")`.
+This is usually `~/.julia/environments` or `C:\Users\YourUsername\.julia\environments`.
+
+It is generally not recommended to heavily populate the default version-specific environments. Rather use project specific environments
+by using changing directories to your project folder and then `] activate`.
+
+```julia
+julia> environments_dir = joinpath(first(DEPOT_PATH), "environments")
+"C:\\Users\\kittisopikulm\\.julia\\environments"
+
+julia> readdir(environments_dir)
+5-element Vector{String}:
+ "__pluto_boot_1.6.0"
+ "v1.5"
+ "v1.6"
+ "v1.7"
+ "v1.8"
+
+julia> v17_project = joinpath(environments_dir, "v1.7", "Project.toml")
+"C:\\Users\\kittisopikulm\\.julia\\environments\\v1.7\\Project.toml"
+
+julia> v17_manifest = joinpath(environments_dir, "v1.7", "Manifest.toml")
+"C:\\Users\\kittisopikulm\\.julia\\environments\\v1.7\\Manifest.toml"
+
+julia> v18_project = joinpath(environments_dir, "v1.8", "Project.toml")
+"C:\\Users\\kittisopikulm\\.julia\\environments\\v1.8\\Project.toml"
+
+julia> v18_manifest = joinpath(environments_dir, "v1.8", "Manifest.toml")
+"C:\\Users\\kittisopikulm\\.julia\\environments\\v1.8\\Manifest.toml"
+
+julia> timestamp = Dates.format(Dates.now(), "yyyy-mm-dd_HH_MM_SS")
+"2022-02-03_16_30_38"
+
+julia> backup_dir = joinpath(dirname(v18_project), "backups", timestamp)
+
+julia> mkpath(backup_dir)
+"C:\\Users\\kittisopikulm\\.julia\\environments\\v1.8\\backups\\2022-02-03_16_30_38"
+
+julia> isfile(v18_project) && mv(v18_project, joinpath(backup_dir, "Project.toml"))
+false
+
+julia> isfile(v18_manifest) && mv(v18_manifest, joinpath(backup_dir, "Manifest.toml"))
+"C:\\Users\\kittisopikulm\\.julia\\environments\\v1.8\\backups\\2022-02-03_16_30_38\\Manifest.toml"
+
+julia> using Pkg
+
+julia> Pkg.activate(v18_project)
+
+julia> Pkg.resolve()
+
+julia> Pkg.instantiate()
+
+```
--- src/EnvironmentMigrators.jl
+++ src/EnvironmentMigrators.jl
@@ -1,5 +1,164 @@
+"""
+Welcome to EnvironmentMigrators.jl.
+
+Use `EnvironmentMigrators.wizard()` for interactive use.
+See also the following functions which are not exported.
+* list_shared_environments
+* select_shared_environments
+* backup_current_environment
+* migrate_selected_environment
+* wizard
+"""
 module EnvironmentMigrators
 
-# Write your package code here.
+using Pkg
+using REPL.TerminalMenus
+using Dates
+
+"""
+    list_shared_environments([depot])
+
+List the top level shared environments in $(joinpath(DEPOT_PATH[1], "environments"))
+"""
+function list_shared_environments(depot = first(DEPOT_PATH))
+    readdir(joinpath(depot, "environments"))
+end
+
+function select_shared_environments(depot = first(DEPOT_PATH))
+    envs = list_shared_environments()
+    options = copy(envs)
+    for idx in eachindex(options)
+        if options[idx] == "v$(VERSION.major).$(VERSION.minor)"
+            options[idx] = options[idx] * "(current version)"
+        end
+    end
+    push!(options, "Backup the current environment: $(Base.active_project()).")
+    push!(options, "Quit. Do Nothing.")
+    menu = RadioMenu(options)
+    println("Use the arrow keys to move the cursor. Press enter to select.")
+    println("Please select a shared environment to copy to $(Base.active_project()):")
+    menu_idx = request(menu)
+    if menu_idx <= length(envs)
+        return joinpath(depot, "environments", envs[menu_idx])
+    elseif menu_idx == length(envs) + 1
+        return Base.active_project()
+    elseif menu_idx == length(options)
+        @info "Quiting. No action taken."
+        return ""
+    end
+end
+
+"""
+    backup_currernt_environments([depot]; fileaction=cp)
+
+Backup the Project.toml and Manifest.toml to `backups/timestamp` where
+`timestamp = Dates.format(Dates.now(), "yyyy-mm-dd_HH_MM_SS")``.
+
+`fileaction` is a keyword parameter for a function that accepts two parameters.
+`fileaction` defaults to `cp` for copy. An alternative is `mv` for move.
+"""
+function backup_current_environment(; fileaction=cp)
+    @info "Current environment:"
+    Pkg.status()
+    current_env_project_toml = Base.active_project()
+    current_dir = dirname(current_env_project_toml)
+    current_env_manifest_toml = joinpath(current_dir, "Manifest.toml")
+    mkpath(current_dir)
+    if isfile(current_env_project_toml) || isfile(current_env_manifest_toml)
+        timestamp = Dates.format(Dates.now(), "yyyy-mm-dd_HH_MM_SS")
+        backup_dir = joinpath(current_dir, "backups", timestamp)
+        mkpath(backup_dir)
+        @info "Backing up Project.toml and Manifest.toml" current_env_project_toml current_env_manifest_toml backup_dir
+        if isfile(current_env_project_toml)
+            fileaction(current_env_project_toml, joinpath(backup_dir, "Project.toml"))
+        else
+            @info "Project.toml does not exist" current_env_project_toml
+        end
+        if isfile(current_env_manifest_toml)
+            fileaction(current_env_manifest_toml, joinpath(backup_dir, "Manifest.toml"))
+        else
+            @info "Manifest.toml does not exist" current_env_manifest_toml
+        end
+    else
+        @info "No existing Project.toml or Manifest.toml to backup" current_env_project_toml current_env_manifest_toml
+    end
+    nothing
+end
+
+"""
+    migrate_selected_environment(selected_env)
+
+Migrate the selected environment specified as a directory in selected_env to
+the current active project.
+"""
+function migrate_selected_environment(selected_env; backup = true)
+    if backup
+        @info "Backing up the current environment" selected_env
+        backup_current_environment(; fileaction=mv)
+    end
+
+    selected_project_toml = joinpath(selected_env, "Project.toml")
+    selected_manifest_toml = joinpath(selected_env, "Manifest.toml")
+    current_env = dirname(Base.active_project())
+    if isfile(selected_project_toml)
+        @info "Copying selected Project.toml to current environment" selected_project_toml current_env
+        cp(selected_project_toml, joinpath(current_env, "Project.toml"))
+    else
+        @info "Selected Project.toml does not exist" selected_project_toml
+    end
+    if isfile(selected_manifest_toml)
+        #@info "Copying selected Manifest.toml to current directory"
+        #cp(selected_project_toml, joinpath(current_env, "Manifest.toml"))
+    else
+        @info "Selected Manifest.toml does not exist" selected_manifest_toml
+    end
+    try
+        Pkg.status()
+        Pkg.resolve()
+        try
+            Pkg.instantiate()
+        catch err
+            @error "An error occurred during Pkg.resolve()." err
+            @info "Run `using Pkg; pkg\"instantiate\"` to view the error."
+            @info "Run `using Pkg; rm\"PkgName\"` to remove problematic packages."
+            return nothing
+        end
+    catch err
+        @error "An error occurred during Pkg.resolve()." err
+        @info "Run `using Pkg; Pkg.resolve()` to view the error."
+        @info "Run `using Pkg; rm\"PkgName\"` to remove problematic packages."
+        @info "Run `using Pkg; Pkg.instantiate()` afterwards."
+        return nothing
+    end
+    @info "Migration successful"
+    Pkg.status()
+end
+
+"""
+    wizard([depot])
+
+Run an interactive terminal based wizard.
+"""
+function wizard(depot = first(DEPOT_PATH))
+    selected_env = select_shared_environments(depot)
+    if isempty(selected_env)
+        return nothing
+    end
+    fileaction = if selected_env == Base.active_project()
+        @info "Backing up the current environment" Base.active_project()
+        cp
+    else
+        mv
+    end
+    backup_current_environment(; fileaction)
+    if selected_env != Base.active_project()
+        migrate_selected_environment(selected_env; backup = false)
+    end
+    return nothing
+end
+
+function __init__()
+    @info "Thank you for using EnvironmentMigrators.jl. Please run `EnvironmentMigrators.wizard()` to begin."
+end
 
 end
