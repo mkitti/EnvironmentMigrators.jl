dac72a7c3426f472ee37087b1c8aa163ff59ca43
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-02-03 18:45:32 -0500

    Add additional delays

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ Pkg.add("Libdl")
     Pkg.add("REPL")
     @test EnvironmentMigrators.list_shared_environments() isa Vector{String}
     @test EnvironmentMigrators.backup_current_environment() === nothing
+    sleep(2)
     @test EnvironmentMigrators.migrate_selected_environment(Base.active_project()) === nothing
     sleep(2)
     @test EnvironmentMigrators.migrate_selected_environment(mktempdir(); backup = true) === nothing
