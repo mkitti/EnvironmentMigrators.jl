c1041a3d46cd571875ed1140413be038656df714
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-02-03 18:42:36 -0500

    Expand testing to windows and mac os, improve test

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,7 @@ Pkg.add("Libdl")
     @test EnvironmentMigrators.list_shared_environments() isa Vector{String}
     @test EnvironmentMigrators.backup_current_environment() === nothing
     @test EnvironmentMigrators.migrate_selected_environment(Base.active_project()) === nothing
+    sleep(2)
     @test EnvironmentMigrators.migrate_selected_environment(mktempdir(); backup = true) === nothing
     @test EnvironmentMigrators.migrate_selected_environment(mktempdir(); backup = false) === nothing
 end
